d1b18ad16470413373968c500593ee678fa4066c
Patrick Meenan <PatMeenan@gmail.com>
2023-03-30 14:10:28 +0000

    Handle normalized accept-encoding

--- bundle.php
+++ bundle.php
@@ -9,7 +9,16 @@ header('Content-Type: application/javascript; charset=UTF-8', true);
 header('Vary: Accept-Encoding,Sec-Available-Dictionary', true);
 header("X-Accept-Encoding-Received: {$_SERVER['HTTP_ACCEPT_ENCODING']}", true);
 $comp = '';
-if (strpos($_SERVER['HTTP_ACCEPT_ENCODING'], 'sbr') !== false &&
+// Go through all of the request headers to see if any have "Accept-Encoding" and "sbr" in them.
+// Some CDN's will rewrite the original Accept-Encoding header from the browser.
+$supports_sbr = false;
+foreach ($_SERVER as $key => $value) {
+  if (stripos($key, 'ACCEPT_ENCODING') !== false && strpos($value, 'sbr') !== false) {
+    $supports_sbr = true;
+    break;
+  }
+}
+if ($supports_sbr &&
     isset($_SERVER['HTTP_SEC_AVAILABLE_DICTIONARY']) &&
     $_SERVER['HTTP_SEC_AVAILABLE_DICTIONARY'] == '74b856e554018fec0d6054c51bc1588fbf2386338d851842447a9510db015732') {
   header("Content-Encoding: sbr", true);
